2503166a3b56f634d24c83105377c78cb5c20aed
omerha <omerha@mta.ac.il>
2019-08-24 16:37:13 +0300

    start of factory

---
+++ C19 Ex01 Omer 204059331 Andrey 321082513.sln/TopFriendsForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C19_Ex01_Omer_204059331_Andrey_321082513.sln
+{
+    public partial class TopFriendsForm : Form
+    {
+        public TopFriendsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void topFriendsListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ C19 Ex01 Omer 204059331 Andrey 321082513.sln/TopWantedItemFormFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C19_Ex01_Omer_204059331_Andrey_321082513.sln
+{
+    public interface ITopWantedItemForm { }
+    public static class TopWantedItemFormFactory
+    {
+        public static ITopWantedItemForm Build(string i_WantedItem)
+        {
+            ITopWantedItemForm res = null;
+            switch (i_WantedItem)
+            {
+
+            }
+            return res;
+        }
+    }
+}
